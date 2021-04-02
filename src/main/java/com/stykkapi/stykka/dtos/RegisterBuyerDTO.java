747cdd213065c317040da350d88852b3acd6e5d6
CodaGott <talk2don90@gmail.com>
2021-04-02 19:20:12 +0100

    added exception, buyer and seller dtos

--- src/main/java/com/stykkapi/stykka/dtos/RegisterBuyerDTO.java
+++ src/main/java/com/stykkapi/stykka/dtos/RegisterBuyerDTO.java
@@ -4,4 +4,8 @@ import lombok.Data;
 
 @Data
 public class RegisterBuyerDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
 }
--- src/main/java/com/stykkapi/stykka/exceptions/Userexceptions.java
+++
@@ -1,4 +0,0 @@
-package com.stykkapi.stykka.exceptions;
-
-public class Userexceptions {
-}
