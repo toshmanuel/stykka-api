01e2a7b689bcd05578734c4ed9b99bbb99ba24be
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2021-04-02 13:28:22 +0100

    user repository

--- src/main/java/com/stykkapi/stykka/repositories/UserRepository.java
+++ src/main/java/com/stykkapi/stykka/repositories/UserRepository.java
@@ -1,4 +1,8 @@
 package com.stykkapi.stykka.repositories;
 
-public interface UserRepository {
+
+import com.stykkapi.stykka.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
 }
