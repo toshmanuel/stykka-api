135105c495b2436b4076763acee0e5819ee7b546
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2021-04-02 21:41:10 +0100

    sellerdto

--- src/main/java/com/stykkapi/stykka/dtos/RegisterSellerDTO.java
+++ src/main/java/com/stykkapi/stykka/dtos/RegisterSellerDTO.java
@@ -1,4 +1,17 @@
 package com.stykkapi.stykka.dtos;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
 public class RegisterSellerDTO {
+
+    private String sellerFirstName;
+    private String sellerLastName;
+    private String sellerEmail;
+    private String sellerPassword;
+    private String storeName;
+    private String bankName;
+    private String accountNumber;
 }
